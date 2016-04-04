0c8fed7e60c76bb11809819d2a769c9f66a2db5b
zijiao <wu+zi=JIAO12>
2016-04-04 11:20:39 +0800

    add service

---
+++ app/src/main/java/pk/service/entity/BaseData.java
@@ -0,0 +1,15 @@
+package pk.service.entity;
+
+import java.io.Serializable;
+
+/**
+ * @version 16/4/4
+ * @user zijiao
+ * @Mark
+ */
+public class BaseData implements Serializable {
+
+    public String status;
+    public String msg;
+
+}
---
+++ app/src/main/java/pk/service/entity/User.java
@@ -0,0 +1,24 @@
+package pk.service.entity;
+
+/**
+ * @version 16/4/4
+ * @user zijiao
+ * @Mark 用户信息
+ */
+public class User extends BaseData {
+
+    private Result result;
+
+    public Result getResult() {
+        if (result == null) {
+            result = new Result();
+        }
+        return result;
+    }
+
+    public static class Result {
+        public String username;
+        public String password;
+    }
+
+}
